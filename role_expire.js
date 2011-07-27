a3e04a84b624c278200786c371d58d3fd85cd8e9
Stewart Robinson <stewsnooze@gmail.com>
2011-07-27 16:26:06 +0100

    Patch from sharplesa to address 915334

--- role_expire.js
+++ role_expire.js
@@ -6,31 +6,35 @@
  * Set of jQuery related routines.
  */
 
-$(document).ready(function() {
-  $('input.role-expire-role-expiry').parent().hide();
+(function ($) {
   
-  $('.role-expire-roles input.form-checkbox').each(function() {
-    var textfieldId = this.id.replace("roles", "role-expire");
+  $(document).ready(function() {
+    $('input.role-expire-role-expiry').parent().hide();
 
-    // Move all expiry date fields under corresponding checkboxes
-    $(this).parent().after($('#'+textfieldId).parent());
-    
-    // Show all expiry date fields that have checkboxes checked
-    if ($(this).attr("checked")) {  
-      $('#'+textfieldId).parent().show();
-    }
-   
+    $('#edit-roles input.form-checkbox').each(function() {
+      var textfieldId = this.id.replace("roles", "role-expire");
+
+      // Move all expiry date fields under corresponding checkboxes
+      $(this).parent().after($('#'+textfieldId).parent());
+
+      // Show all expiry date fields that have checkboxes checked
+      if ($(this).attr("checked")) {
+        $('#'+textfieldId).parent().show();
+      }
+
+    });
+
+    $('#edit-roles input.form-checkbox').click(function() {
+      var textfieldId = this.id.replace("roles", "role-expire");
+
+      // Toggle expiry date fields
+      if ($(this).attr("checked")) {
+        $('#'+textfieldId).parent().show(200);
+      }
+      else {
+        $('#'+textfieldId).parent().hide(200);
+      }
+    });
   });
-  
-  $('.role-expire-roles input.form-checkbox').click(function() {
-    var textfieldId = this.id.replace("roles", "role-expire");
-    
-    // Toggle expiry date fields
-    if ($(this).attr("checked")) {
-      $('#'+textfieldId).parent().show(200);
-    } 
-    else {
-      $('#'+textfieldId).parent().hide(200);    
-    }
-  });
-});
+
+})(jQuery);

55e5627a2f82c70cce4054271ee3ac5a32adc625
sharplesa <sharplesa@153911.no-reply.drupal.org>
2011-08-10 08:17:44 +0100

    Patch from sharplesa to address 915334 with Drupal behaviours e.t.c.

--- role_expire.js
+++ role_expire.js
@@ -6,35 +6,38 @@
  * Set of jQuery related routines.
  */
 
+// See http://drupal.org/node/756722 for conversion to D7 and Drupal.behaviors.
+
 (function ($) {
   
-  $(document).ready(function() {
-    $('input.role-expire-role-expiry').parent().hide();
+  Drupal.behaviors.role_expire = {
+    attach: function (context, settings) {
+      $('input.role-expire-role-expiry', context).parent().hide();
 
-    $('#edit-roles input.form-checkbox').each(function() {
-      var textfieldId = this.id.replace("roles", "role-expire");
+      $('#edit-roles input.form-checkbox', context).each(function() {
+        var textfieldId = this.id.replace("roles", "role-expire");
 
-      // Move all expiry date fields under corresponding checkboxes
-      $(this).parent().after($('#'+textfieldId).parent());
+        // Move all expiry date fields under corresponding checkboxes
+        $(this).parent().after($('#'+textfieldId).parent());
 
-      // Show all expiry date fields that have checkboxes checked
-      if ($(this).attr("checked")) {
-        $('#'+textfieldId).parent().show();
-      }
+        // Show all expiry date fields that have checkboxes checked
+        if ($(this).attr("checked")) {
+          $('#'+textfieldId).parent().show();
+        }
+      });
 
-    });
+      $('#edit-roles input.form-checkbox', context).click(function() {
+        var textfieldId = this.id.replace("roles", "role-expire");
 
-    $('#edit-roles input.form-checkbox').click(function() {
-      var textfieldId = this.id.replace("roles", "role-expire");
-
-      // Toggle expiry date fields
-      if ($(this).attr("checked")) {
-        $('#'+textfieldId).parent().show(200);
-      }
-      else {
-        $('#'+textfieldId).parent().hide(200);
-      }
-    });
-  });
+        // Toggle expiry date fields
+        if ($(this).attr("checked")) {
+          $('#'+textfieldId).parent().show(200);
+        }
+        else {
+          $('#'+textfieldId).parent().hide(200);
+        }
+      });
+    }
+  }
 
 })(jQuery);
